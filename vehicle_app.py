beba2e66f61775faa354a5bd27db7fe342c54d3d
Kaushik Hariharan <hkaushik58@gmail.com>
2025-03-11 17:09:47 -0700

    Update vehicle_app.py AGAIN!

--- vehicle_app.py
+++ vehicle_app.py
@@ -18,8 +18,10 @@ class VehicleApp(QDialog):
         self.ui = uic.loadUi('ev_gui.ui')
 
         # Student menu and query button event handlers.
-        self.ui.checkBox.currentIndexChanged.connect(self._initialize_table)
-        self.ui.radioButton.clicked.connect(self._enter_class_data)
+        # self.ui.checkBox.currentIndexChanged.connect(self._initialize_table)
+        self.ui.checkBox.currentIndexChanged.connect(self._station_status_date_info)
+        self.ui.radioButton.clicked.connect(self._low_category_cars)
+        self.ui.radioButton.clicked.connect(self._new_cars)
         
         # Initialize the teacher menu and the student table.
         self._low_category_cars()
@@ -57,9 +59,14 @@ class VehicleApp(QDialog):
         conn.close()
 
         # Set the menu items to the students' names.
+        # for row in rows:
+        #     name = row[0] + ' ' + row[1]
+        #     self.ui.Cars_Info.addItem(row) 
+
+        self.Cars_Info.clear()
+        
         for row in rows:
-            name = row[0] + ' ' + row[1]
-            self.ui.Cars_Info.addItem(row)  
+            self.Cars_Info.addItem(f"{row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]}")
 
     def _new_cars(self):
         conn = db_connection(config_file = 'querycrew.ini')
@@ -83,9 +90,13 @@ class VehicleApp(QDialog):
         conn.close()
 
         # Set the menu items to the students' names.
+        # for row in rows:
+        #     name = row[0] + ' ' + row[1]
+        #     self.ui.Cars_Info.addItem(row)  
+        self.Cars_Info.clear()
+        
         for row in rows:
-            name = row[0] + ' ' + row[1]
-            self.ui.Cars_Info.addItem(row)  
+            self.Cars_Info.addItem(f"{row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]}")
 
     def _station_status_date_info(self):
         conn = db_connection(config_file = 'querycrew.ini')
@@ -106,74 +117,14 @@ class VehicleApp(QDialog):
         conn.close()
 
         # Set the menu items to the students' names.
+        # for row in rows:
+        #     name = row[0] + ' ' + row[1]
+        #     self.ui.Station_Info.addItem(row)  
+
+        self.Station_Info.clear()
+        
         for row in rows:
-            name = row[0] + ' ' + row[1]
-            self.ui.Station_Info.addItem(row)  
-
-    
-    # def _adjust_column_widths(self):
-    #     """
-    #     Adjust the column widths of the class table to fit the contents.
-    #     """
-    #     header = self.ui.class_table.horizontalHeader();
-    #     header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
-    #     header.setSectionResizeMode(1, QHeaderView.Stretch)
-        
-    # def _initialize_table(self):
-    #     """
-    #     Clear the table and set the column headers.
-    #     """
-    #     self.ui.class_table.clear()
-
-    #     col = ['  Code  ', '   Subject   ']
-    #     self.ui.class_table.setHorizontalHeaderLabels(col)        
-    #     self._adjust_column_widths()
-        
-    # def _enter_class_data(self):    
-    #     """
-    #     Enter class data from the query into 
-    #     the student and takes tables.
-    #     """    
-    #     name = self.ui.student_menu.currentData()
-    #     first_name = name[0]
-    #     last_name = name[1]
-        
-    #     conn = db_connection(config_file = 'school.ini')
-    #     cursor = conn.cursor()
-        
-    #     sql = ( 
-    #         f"""
-    #         SELECT class.code, class.subject
-    #         FROM student
-    #         JOIN takes
-    #           ON takes.student_id = student.id
-    #         JOIN class
-    #           ON class.code = takes.class_code
-    #         WHERE student.last = '{last_name}'
-    #           AND student.first = '{first_name}'
-    #         ORDER BY class.code
-    #         """ 
-    #           )    
-    
-    #     cursor.execute(sql)
-    #     rows = cursor.fetchall()
-              
-    #     cursor.close()
-    #     conn.close()
-       
-    #     # Set the class data into the table cells.
-    #     row_index = 0
-    #     for row in rows:
-    #         column_index = 0
-            
-    #         for data in row:
-    #             item = QTableWidgetItem(str(data))
-    #             self.ui.class_table.setItem(row_index, column_index, item)
-    #             column_index += 1
-    
-    #         row_index += 1
-               
-    #     self._adjust_column_widths()
+            self.Station_Info.addItem(f"{row[0]} - {row[1]} - {row[2]}")
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
